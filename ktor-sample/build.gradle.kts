91d7eb2f25d8bc90521d5cefba4ee794d00cbc04
Kevin Nguyen <knnguyen265@gmail.com>
2026-04-16 22:16:17 +0100

    Backend partially complete, models setup

--- ktor-sample/build.gradle.kts
+++ ktor-sample/build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.config.yaml)
+    implementation(libs.exposed.java.time)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
